4c8323b9ab17607d3d378f638d6095d56aaa8d1b
bandarisravan-28 <bandarisravan28@gmail.com>
2026-09-21 12:34:08 +0530

    adding tf

---
+++ resoure.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "web01" {
+  name     = var.rg01
+  location = var.location
+}
---
+++ var.tf
@@ -0,0 +1,12 @@
+
+variable "rg01" {
+  description = "Name of the Resource Group"
+  type        = string
+  default     = "rg-demo"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
